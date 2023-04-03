b1ba7c4878469fa5ff0c0fe6b84ec52224810df9
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:29:30 -0300

    feat: add navbar bare bones

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar"></div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: block;
+  height: 5vh;
+  width: 100vw;
+  background-color: #13689E;
+}
+</style>
