6d44e52d1a1e1f101610ba974f95bb0322bdd8fb
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:30:05 -0300

    feat: add SearchTitle component bare bones

---
+++ src/components/home/SearchTitle.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h2 class="text-h2 text-uppercase text-center">My Games List</h2>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
