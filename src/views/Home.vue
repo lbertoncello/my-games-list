4ed0c01969771bdd260e57ceeaeffe68313071e3
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:32:33 -0300

    feat: add Home view bare bones

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <HelloWorld />
+  <home-search />
+  <home-info-banner />
+  <hello-world />
 </template>
 
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
+import HomeSearch from '@/components/home/Search.vue';
+import HomeInfoBanner from '@/components/home/InfoBanner.vue';
 </script>
