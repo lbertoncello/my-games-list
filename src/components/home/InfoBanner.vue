eb2b84d5bde083d477c19e2b8060be524d00119a
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:31:44 -0300

    feat: add InfoBanner component bare bones

---
+++ src/components/home/InfoBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+    <v-container class="home-info-banner" fluid>
+      <v-row>
+        <v-col>
+          <h3 class="text-h3 text-center">My Games List is...</h3>
+        </v-col>
+      </v-row>
+    </v-container>
+  </article>
+</template>
+
+<style scoped>
+.home-info-banner {
+  height: 20vh;
+  background: #50A7DE;
+}
+</style>
