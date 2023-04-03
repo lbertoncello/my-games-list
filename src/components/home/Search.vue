e03b51cd895288f0f605a5cb9d984ba2fb6815bb
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:31:19 -0300

    feat: add HomeSearch component bare bones

---
+++ src/components/home/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <article>
+    <v-container class="search-section" fluid>
+      <v-row align="center" style="height: 100%;">
+        <v-col>
+          <search-background class="search-background" />
+          <div class="search-section-content">
+            <search-title />
+            <search-bar />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </article>
+</template>
+
+<script setup>
+import SearchTitle from './SearchTitle.vue';
+import SearchBar from './SearchBar.vue';
+import SearchBackground from './SearchBackground.vue';
+</script>
+
+<style scoped>
+.search-section {
+  height: 75vh;
+}
+
+.search-section {
+  overflow: hidden;
+  position: relative;
+}
+
+.search-section-content {
+  position: relative;
+}
+
+.search-background {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: auto;
+  opacity: 0.6;
+}
+</style>
