7cfb8eeb5b9ef93115f6c6ce7f93a3d684628e0c
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-03 19:30:59 -0300

    feat: add SearchBar component bare bones

---
+++ src/components/home/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="2"></v-col>
+        <v-col cols="8">
+          <v-text-field
+            label="Search for your game"
+            variant="outlined"
+            prepend-inner-icon="mdi-magnify"
+            clearable
+          />
+        </v-col>
+        <v-col cols="2"></v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
