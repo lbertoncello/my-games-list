2709c781f7ac8558d1805db189c6c34c4929f2c3
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-04 20:39:03 -0300

    feat: add opacity to background image

--- src/components/home/Search.vue
+++ src/components/home/Search.vue
@@ -25,43 +25,25 @@ import SearchBar from './SearchBar.vue';
 <style scoped>
 .search-section {
   height: 80vh;
-  /* Add a background image that fills all the available space */
-  background: url("@/assets/images/home/dark-souls-002.jpg") no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  position: relative;
 }
 
-.search-section {
-  overflow: hidden;
-  position: relative;
+.search-section:before {
+  content: ' ';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.65;
+  /* Add a background image that fills all the available space */
+  background: url("@/assets/images/home/dark-souls-002.jpg") no-repeat center center fixed;
+  background-size: cover;
 }
 
 .search-section-content {
   position: relative;
-}
-
-.search-background {
-   /* Set rules to fill background */
-  min-height: 100%;
-  min-width: 1024px;
-
-  /* Set up proportionate scaling */
-  width: 100%;
-  height: auto;
-
-  /* Set up positioning */
-  position: fixed;
-  top: 0;
-  left: 0;
-}
-
-@media screen and (min-width: 1024px) {
-  .search-background {
-    left: 50%;
-    margin-left: -512px;   /* 50% */
-  }
 }
 
 @media screen and (min-width: 768px) {
@@ -69,4 +51,5 @@ import SearchBar from './SearchBar.vue';
     height: 75vh;
   }
 }
+
 </style>
