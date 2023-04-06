725c894ff361be4b4badd391d4597c573f35a38a
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-06 20:53:12 -0300

    feat: add options to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,55 @@
 <template>
   <nav class="navbar">
-    <div class="navbar"></div>
+    <v-navigation-drawer
+      v-model="sidebar"
+      disable-resize-watcher
+    >
+    <v-list>
+        <v-list :items="navbarItems"></v-list>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar>
+      <span class="d-sm-none">
+        <v-app-bar-nav-icon @click="sidebar = !sidebar">
+        </v-app-bar-nav-icon>
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="d-none d-sm-flex">
+        <v-btn
+          flat
+          v-for="item in navbarItems"
+          :key="item.title"
+          :to="item.value">
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </nav>
 </template>
 
-<style scoped>
-.navbar {
-  display: block;
-  height: 10vh;
-  width: 100vw;
-  background-color: #090021;
-}
+<script setup>
+import { ref } from 'vue';
 
-@media screen and (min-width: 768px) {
-  .navbar {
-    height: 5vh;
-  }
+const sidebar = ref(false);
+
+const navbarItems = [
+  { title: 'Search', value: '/search' },
+  { title: 'Platforms', value: '/search' },
+  { title: 'Popular', value: '/popular' },
+  { title: 'Sign Up', value: '/signup' },
+  { title: 'Sign In', value: '/signin' },
+];
+const appTitle = 'MY GAMES LIST';
+</script>
+
+<style scoped>
+.navbar > * {
+  background-color: #090021;
 }
 </style>
