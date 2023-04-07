609449869f06668cce0dab291fb2e0e9265d86bb
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-06 22:19:35 -0300

    feat: update navbar responsiveness

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,32 +4,61 @@
       v-model="sidebar"
       disable-resize-watcher
     >
-    <v-list>
-        <v-list :items="navbarItems"></v-list>
-      </v-list>
+      <v-list :items="navbarItems"></v-list>
     </v-navigation-drawer>
 
-    <v-toolbar>
-      <span class="d-sm-none">
-        <v-app-bar-nav-icon @click="sidebar = !sidebar">
-        </v-app-bar-nav-icon>
-      </span>
+  <v-toolbar>
+    <span class="d-md-none">
+      <v-app-bar-nav-icon @click="sidebar = !sidebar">
+      </v-app-bar-nav-icon>
+    </span>
+
+    <v-spacer />
+
+    <div class="navbar-title">
       <v-toolbar-title>
         <router-link to="/" style="cursor: pointer">
-          {{ appTitle }}
+          <figure>
+            <v-img
+              src="@/assets/images/mal.png"
+              cover
+            />
+          </figure>
         </router-link>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="d-none d-sm-flex">
-        <v-btn
-          flat
-          v-for="item in navbarItems"
-          :key="item.title"
-          :to="item.value">
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    </div>
+
+    <v-spacer />
+
+    <v-toolbar-items class="d-none d-md-flex">
+      <v-btn
+        flat
+        v-for="item in navbarFirstItems"
+        :key="item.title"
+        :to="item.value"
+      >
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-spacer />
+    <v-spacer />
+    <v-spacer />
+
+    <v-toolbar-items class="d-none d-sm-flex">
+      <v-btn
+        flat
+        v-for="item in navbarLastItems"
+        :key="item.title"
+        :to="item.value"
+      >
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-spacer />
+  </v-toolbar>
+
   </nav>
 </template>
 
@@ -38,18 +67,27 @@ import { ref } from 'vue';
 
 const sidebar = ref(false);
 
-const navbarItems = [
-  { title: 'Search', value: '/search' },
-  { title: 'Platforms', value: '/search' },
+const navbarFirstItems = ref([
+  { title: 'Statistics', value: '/statistics' },
+  { title: 'Platforms', value: '/platforms' },
   { title: 'Popular', value: '/popular' },
+]);
+
+const navbarLastItems = ref([
   { title: 'Sign Up', value: '/signup' },
   { title: 'Sign In', value: '/signin' },
-];
-const appTitle = 'MY GAMES LIST';
+]);
+
+const navbarItems = ref([...navbarFirstItems.value, ...navbarLastItems.value]);
+
 </script>
 
 <style scoped>
 .navbar > * {
   background-color: #090021;
+}
+
+.navbar-title > * {
+  width: 10rem;
 }
 </style>
