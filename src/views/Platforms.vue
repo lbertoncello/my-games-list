b7c2bf86df358039806f24ac15376f5d3978665f
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-10 18:12:33 -0300

    feat: add views boilerplate

---
+++ src/views/Platforms.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="platforms">
+    <v-container fluid>
+      <v-row no-gutters>
+        <v-col>
+          <h1>Platforms</h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </article>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="sign-up">
+    <v-container fluid>
+      <v-row no-gutters>
+        <v-col>
+          <h1>Sign Up</h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </article>
+</template>
+
+<script setup>
+
+</script>
