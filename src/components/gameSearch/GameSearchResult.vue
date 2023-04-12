f8e72af758c6a1eb9298bc4f953065796c9f7ab2
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-12 17:38:55 -0300

    feat: add component GameSearchResult

---
+++ src/components/gameSearch/GameSearchResult.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="result">
+    <v-container
+      fluid
+    >
+      <v-row
+        no-gutters
+        align="center"
+        style="height: 100%;"
+      >
+        <v-col class="d-flex justify-center">
+          <h1>Search Result</h1>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col class="d-flex justify-center">
+          <GameCard />
+        </v-col>
+      </v-row>
+    </v-container>
+  </article>
+</template>
+
+<script setup>
+import GameCard from './GameCard.vue';
+</script>
