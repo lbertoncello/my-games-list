eede1525d06b03d0b63931cb134d5759f26eab12
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-14 20:40:11 -0300

    feat: add component GameRating

---
+++ src/components/gameSearch/GameRating.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="game-rating">
+    <div class="game-rating-circle">
+      <div class="game-rating-rate">80</div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.game-rating-circle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #4BB543;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 100%;
+  box-shadow: 1px 1px 1px #383838;
+  border: 1px solid #30752B;
+}
+</style>
