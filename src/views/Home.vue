0e8eebab1b6a02791c3229c69667ea80b9af4ce3
lbertoncello <lucas_b_andrade@hotmail.com>
2023-04-17 19:27:21 -0300

    refactor: remove useless component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 <template>
   <HomeSearch />
   <HomeInfoBanner />
-  <HelloWorld />
 </template>
 
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue';
 import HomeSearch from '@/components/home/Search.vue';
 import HomeInfoBanner from '@/components/home/InfoBanner.vue';
 </script>
