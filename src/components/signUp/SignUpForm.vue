c6422e620b90a492049b7d0fdbdc0eb02edf9db5
lbertoncello <lucas_b_andrade@hotmail.com>
2023-06-16 20:18:57 -0300

    feat: add sign up form structure and styles

--- src/components/signUp/SignUpForm.vue
+++ src/components/signUp/SignUpForm.vue
@@ -1,1 +1,107 @@
-/* eslint no-use-before-define: 0 */
+<template>
+  <section class="sign-up-form elevation-5">
+    <v-form v-model="form" @submit.prevent="">
+      <v-container>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field
+              type="text"
+              v-model="name"
+              :rules="[rules.required]"
+              density="compact"
+              label="Name"
+              placeholder="Your name"
+              prepend-inner-icon="mdi-account"
+              clearable
+            />
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field
+              type="email"
+              v-model="email"
+              :rules="[rules.required]"
+              density="compact"
+              label="Email"
+              placeholder="Your email"
+              prepend-inner-icon="mdi-email-outline"
+              clearable
+            />
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field
+              type="password"
+              v-model="password"
+              :rules="[rules.required]"
+              density="compact"
+              label="Password"
+              placeholder="Type a strong password"
+              hint="Your password must contain..."
+              prepend-inner-icon="mdi-lock"
+              clearable
+            />
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field
+              type="password"
+              v-model="passwordConfirmation"
+              :rules="[rules.required]"
+              density="compact"
+              label="Password Confirmation"
+              placeholder="Type the same password"
+              prepend-inner-icon="mdi-lock-alert"
+              clearable
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-btn
+              :disabled="!form"
+              block
+              color="success"
+              size="large"
+              type="submit"
+              variant="elevated"
+            >
+              Sign Up
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import rules from '@/composables/helpers/fieldValidation';
+
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const passwordConfirmation = ref('');
+const form = ref(false);
+</script>
+
+<style scoped>
+.sign-up-form {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  list-style: none;
+  width: 100%;
+  background: #635985;
+  border-radius: 5px;
+}
+
+.sign-up-form form {
+  width: 100%;
+}
+</style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,13 +1,71 @@
 <template>
   <article class="sign-up">
-    <v-container fluid>
-      <v-row no-gutters>
-        <v-col>
-          <h1>Sign Up</h1>
-        </v-col>
-      </v-row>
-    </v-container>
+    <SignUpTitle />
+    <SignUpForm />
   </article>
 </template>
 
-<script setup></script>
+<script setup>
+import SignUpTitle from '@/components/signUp/SignUpTitle.vue';
+import SignUpForm from '@/components/signUp/SignUpForm.vue';
+</script>
+
+<style scoped>
+.sign-up {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 95vw;
+  margin: auto;
+}
+
+.sign-up > * {
+  margin: 1rem auto 0;
+}
+
+/* SM breakpoint */
+@media only screen and (min-width: 600px) {
+  .sign-up {
+    width: 75vw;
+  }
+}
+
+/* MD breakpoint */
+@media only screen and (min-width: 960px) {
+  .sign-up {
+    width: 40vw;
+  }
+
+  .result-list {
+    justify-content: space-between;
+  }
+
+  .result-list li {
+    max-width: 49%;
+    min-width: 0;
+  }
+}
+
+/* LG breakpoint */
+@media only screen and (min-width: 1280px) {
+  .sign-up {
+    width: 30vw;
+  }
+}
+
+/* XL breakpoint */
+@media only screen and (min-width: 1920px) {
+  .sign-up {
+    width: 25vw;
+  }
+}
+
+/* XXL breakpoint */
+@media only screen and (min-width: 2560px) {
+  .sign-up {
+    width: 15vw;
+  }
+}
+</style>
